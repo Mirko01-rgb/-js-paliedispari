4ed44b7382bcc9b03996aeefd8177b66cc5e418d
Mirko <mirkosbaglia2001@gmail.com>
2021-03-12 18:37:47 +0100

    Primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 // Una funzione per capire se la parola è
 // palindroma
 
+function isPalindroma(word){
+// console.log(word);
+for (var i = 0; i < word.length; i++) {
+  console.log(i, word[i]);
+ 
 
 
+}
 
+}
 
+var worD= prompt('Inserisci una parola:')
+isPalindroma(worD);
 
-
+//OPPURE
+// var worD = isPalindroma(prompt('Inserisci una parola:'));
 
 
 
@@ -25,29 +35,31 @@
 
 //Function
 
-function random(numMax, numMin){
- var rnd = Math.floor(Math.random()* numMax)  + numMin;
- return rnd;
-}
-
-var numRandomici = random(5, 1);
-console.log('Numero randomico', numRandomici);
-
-var sum = 0;
-var utenteNumb = parseInt(prompt('Scegli un numero da 1 a 5:'));
-if(utenteNumb < 0 || utenteNumb > 5){
-  alert('Hai inserito un numero non compreso tra 1 e 5');
-}
-var pariDispari = prompt('Cosa scegli Pari o Dispari:')
-// console.log(utenteNumb);
-// console.log(pariDispari);
-
- sum = utenteNumb + numRandomici;
- console.log('Valore della somma', sum);
- if (sum % 2 == 0 && pariDispari =='Pari'){
-   console.log('Ha vinto Mirko');
- }else if (sum % 2 == 1 && pariDispari == 'Dispari'){
-  console.log('Ha vinto Mirko');
-}else{
-  console.log('Ha vinto il computer');
-}
+// function random(numMax, numMin){
+//  var rnd = Math.floor(Math.random()* numMax)  + numMin;
+//  return rnd;
+// }
+//
+// var numRandomici = random(5, 1);
+// console.log('Numero randomico',numRandomici);
+// // var numRandomic= random(7, 9);
+// // console.log('Numero randomico',numRandomic);
+//
+// var sum = 0;
+// var utenteNumb = parseInt(prompt('Scegli un numero da 1 a 5:'));
+// if(utenteNumb < 0 || utenteNumb > 5){
+//   alert('Hai inserito un numero non compreso tra 1 e 5');
+// }
+// var pariDispari = prompt('Cosa scegli Pari o Dispari:')
+// // console.log(utenteNumb);
+// // console.log(pariDispari);
+//
+//  sum = utenteNumb + numRandomici;
+//  console.log('Valore della somma', sum);
+//  if (sum % 2 == 0 && pariDispari =='Pari'){
+//    console.log('Ha vinto Mirko');
+//  }else if (sum % 2 == 1 && pariDispari == 'Dispari'){
+//   console.log('Ha vinto Mirko');
+// }else{
+//   console.log('Ha vinto il computer');
+// }
